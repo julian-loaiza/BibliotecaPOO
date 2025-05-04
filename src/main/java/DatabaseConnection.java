e7a17fdf7eb4bd5cdceb627d0cb691461ae1e0ed
Rafael0896 <realvarez54@misena.edu.co>
2025-05-03 20:31:28 -0500

    conexión base de datos.

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/new_schema";
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
     private static final String USER = "root";
-    private static final String PASSWORD = "12345";
+    private static final String PASSWORD = "1234";
 
     public static Connection getConnection() throws SQLException {
         try {
